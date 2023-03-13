12bdd3d0491d95ea03877af6d8a99dc1353fd2e9
Christopher Kenny <ctk44@cornell.edu>
2023-03-13 16:20:54 -0400

    update tests

--- tests/testthat/test-get_global_path_of_legend_players.R
+++ tests/testthat/test-get_global_path_of_legend_players.R
@@ -1,4 +1,4 @@
-with_mock_dir("t/global_path_of_legend_players", {
+with_mock_dir("t/global_pol_players", {
   test_that("cr_get_global_path_of_legend_players works", {
     x <- cr_get_global_path_of_legend_players('2023-02', limit = 10)
     expect_s3_class(x, 'tbl_df')
