e6efa0bd98d5d8ed47743e10b6ce9ff0061f9175
Christopher Kenny <ctk44@cornell.edu>
2023-06-26 14:23:09 -0400

    paths

--- tests/testthat/test-get_local_path_of_legend_players.R
+++ tests/testthat/test-get_local_path_of_legend_players.R
@@ -1,4 +1,4 @@
-with_mock_dir("t/local_path_of_legend_players", {
+with_mock_dir("t/l_pol_p", {
  test_that("cr_get_local_path_of_legend_players works", {
    x <- cr_get_local_path_of_legend_players(57000249, limit = 10)
    expect_s3_class(x, 'tbl_df')
